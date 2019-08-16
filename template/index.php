1af01fabadf9de6969b175040ed7ed6d4e286c22
sengngy <sengngy24@gmail.com>
2019-08-16 10:43:03 +0700

    initial

---
+++ template/index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+    <link rel="stylesheet" href="../asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+    <link rel="stylesheet" href="../asset/css/style.css">
+</head>
+
+<body>
+
+    <!-- Menu start -->
+    <div class="menu-top bg-light">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">SportNews</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sports</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Menu End -->
+
+    <!-- big Ads -->
+    <div class="container mb-5 mt-3">
+        <div class="big-ads">
+            <img src="../asset/img/ads1.jpg" alt="" width="100%">
+        </div>
+    </div>
+    <!-- end Big Ads -->
+
+    <!-- Start Content -->
+    <div class="container">
+        <div class="row">
+            <!-- start Conent -->
+            <div class="col-sm-9 content">
+                <!-- Start lestes news  -->
+                <div class="lastes-new">
+                    <!-- lestes new title -->
+                    <div class="header-lestes-new">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h5 class="text-success font-khbat">ព័ត៌មានថ្មីៗ</h5>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#" class="float-right text-success"><i
+                                        class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- End lestes new title -->
+
+                    <!-- Start lestes news item -->
+
+                    <div class="body-item-new body-lastes p-3" style="border-top: solid 2px green;">
+                        <div id="lastes-news-slide" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-interval="2500">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <div class="card">
+                                                <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..."
+                                                    height="130">
+                                                <div class="card-body">
+                                                    <p class="card-title">Card title</p>
+                                                    <p class="card-text">Some quick example text to build on the card
+                                                        title and make
+                                                        up the bulk of the card's content.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="card">
+                                                <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..."
+                                                    height="130">
+                                                <div class="card-body">
+                                                    <p class="card-title">Card title</p>
+                                                    <p class="card-text">Some quick example text to build on the card
+                                                        title and make
+                                                        up the bulk of the card's content.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="card">
+                                                <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..."
+                                                    height="130">
+                                                <div class="card-body">
+                                                    <p class="card-title">Card title</p>
+                                                    <p class="card-text">Some quick example text to build on the card
+                                                        title and make
+                                                        up the bulk of the card's content.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item" data-interval="2500">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <div class="card">
+                                                <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..."
+                                                    height="130">
+                                                <div class="card-body">
+                                                    <p class="card-title">Card title</p>
+                                                    <p class="card-text">Some quick example text to build on the card
+                                                        title and make
+                                                        up the bulk of the card's content.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="card">
+                                                <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..."
+                                                    height="130">
+                                                <div class="card-body">
+                                                    <p class="card-title">Card title</p>
+                                                    <p class="card-text">Some quick example text to build on the card
+                                                        title and make
+                                                        up the bulk of the card's content.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="card">
+                                                <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..."
+                                                    height="130">
+                                                <div class="card-body">
+                                                    <p class="card-title">Card title</p>
+                                                    <p class="card-text">Some quick example text to build on the card
+                                                        title and make
+                                                        up the bulk of the card's content.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- End lestes news item -->
+
+                </div>
+                <!-- End lestes news  -->
+
+                <!-- Start football news -->
+                <div class="football-news  mt-5 mb-5">
+                    <div class="header-lestes-new">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h5 class="text-danger font-khbat">កីឡា <i class="far fa-futbol"></i></h5>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#" class="float-right text-danger"><i
+                                        class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- End football new title -->
+
+                    <!-- Start football news item -->
+
+                    <div class="body-item-new body-football p-3" style="border-top: solid 2px red;">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..." height="130">
+                                    <div class="card-body">
+                                        <p class="card-title">Card title</p>
+                                        <p class="card-text">Some quick example text to build on the card title and make
+                                            up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..." height="130">
+                                    <div class="card-body">
+                                        <p class="card-title">Card title</p>
+                                        <p class="card-text">Some quick example text to build on the card title and make
+                                            up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..." height="130">
+                                    <div class="card-body">
+                                        <p class="card-title">Card title</p>
+                                        <p class="card-text">Some quick example text to build on the card title and make
+                                            up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!--  End football news  -->
+
+                <!-- Start Boxing News -->
+                <div class="football-news  mt-5 mb-5">
+                    <div class="header-lestes-new">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h5 class="text-info font-khbat">ប្រដាល់</h5>
+                            </div>
+                            <div class="col-sm-6">
+                                <a href="#" class="float-right text-info"><i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- End Boxing new title -->
+
+                    <!-- Start Boxing news item -->
+
+                    <div class="body-item-new body-boxing p-3" style="border-top: solid 2px rgb(6, 158, 204);">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..." height="130">
+                                    <div class="card-body">
+                                        <p class="card-title">Card title</p>
+                                        <p class="card-text">Some quick example text to build on the card title and make
+                                            up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..." height="130">
+                                    <div class="card-body">
+                                        <p class="card-title">Card title</p>
+                                        <p class="card-text">Some quick example text to build on the card title and make
+                                            up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="card">
+                                    <img src="../asset/img/ads1.jpg" class="card-img-top" alt="..." height="130">
+                                    <div class="card-body">
+                                        <p class="card-title">Card title</p>
+                                        <p class="card-text">Some quick example text to build on the card title and make
+                                            up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- End Boxing News -->
+
+
+            </div>
+            <!-- End Conent -->
+
+            <!-- Start Right Sidebar -->
+
+            <div class="col-sm-3 right-sidebar">
+                <div class="ads-item mb-3">
+                    <img src="../asset/img/ads2.jpg" alt="" width="100%">
+                </div>
+                <div class="ads-item mb-3">
+                    <img src="../asset/img/ads4.jpg" alt="" width="100%">
+                </div>
+
+                <div class="popular-news">
+                    <div class="header-popular-news">
+                        <p>ព័ត៌មានថ្មីៗ</p>
+                    </div>
+                    <ul class="item-popular-news">
+                        <li><a href="#">ប្រវត្តិ​សាស្ត្រ​ហើយ ​កម្ពុជា ​ទម្លាក់​​វៀតណាម ពី AFF U18</a></li>
+                        <li><a href="#">បUEFA ប្រកាស​ពី​បេក្ខភាព​​ ៣រូប​ចុងក្រោយ​នៃ​ពាន​កីឡាករ​ឆ្នើម​ប្រចាំ​ឆ្នាំ</a>
+                        </li>
+                        <li><a href="#">ប្រវត្តិ​សាស្ត្រ​ហើយ ​កម្ពុជា ​ទម្លាក់​​វៀតណាម ពី AFF U18</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- End Right Sidebar -->
+        </div>
+    </div>
+    <!-- End Content -->
+
+
+</body>
+
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="../asset/js/bootstrap.min.js"></script>
